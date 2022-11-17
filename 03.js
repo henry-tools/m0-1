66386b596b7e521a11f3a41c44ea3ef94632c613
Henry Tools <118113005+henry-tools@users.noreply.github.com>
2022-11-17 10:37:34 -0300

    Add files via upload

---
+++ 03.js
@@ -0,0 +1,21 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function fakeBinary(size) {
+  // La funcion llamada 'fakeBinary' recibe como argumento size(tamaño) que es un numero (entero)
+  // y debe devolver un string de 1s y 0s con el tamaño indicado.
+  // siempre empieza por 1
+  // Por ej:
+  // fakeBinary(10) devuelve "1010101010"
+  // fakeBinary(3) devuelve "101"
+  // Tu código aca:
+
+}
+
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = fakeBinary
---
+++ 04.js
@@ -0,0 +1,19 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function puntosDelEquipo(array) {
+  // la funcion recibe un array con los resultados de los partidos del campeonato de futbol de un equipo
+  // en este formato ["3:1", "2:2", "0:1", ...]
+  //la funcion debe calcular y retornar cuantos puntos consiguio el equipo teniendo en cuenta:
+  //que su resultado es el primero en cada string
+  // un partido ganado suma 3 puntos, empate suma 1 punto, y perder 0!
+  // Tu código aca:
+
+}
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = puntosDelEquipo
